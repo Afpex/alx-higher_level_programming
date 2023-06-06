0fbc26e366b301666eacfd1896bc344885262469
Anthony Ndolo <bondolo90@gmail.com>
2023-06-06 11:30:00 -0700

    All Tasks

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Print ASCII alphabet, in reverse, alternating lowercase & uppercase."""
+j = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - j)), end="")
+    j = 32 if j == 0 else 0
