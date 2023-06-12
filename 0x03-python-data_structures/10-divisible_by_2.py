09d1091fa95911fa4f3e63bc0ad90689e9265596
Anthony Ndolo <bondolo90@gmail.com>
2023-06-12 10:30:58 -0700

    Divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-  if not my_list:
-      return []
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return []
 
   new_list = [element % 2 == 0 for element in my_list]
 
