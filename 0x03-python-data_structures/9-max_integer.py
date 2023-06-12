876da8f4b187e873e12bb91ef40e6048df9e0036
Anthony Ndolo <bondolo90@gmail.com>
2023-06-12 10:35:58 -0700

    Indentation

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,10 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
 
-  max_integer = my_list[0]
-  for integer in my_list:
-      if integer > max_integer:
-          max_integer = integer
+    max_integer = my_list[0]
+    for integer in my_list:
+        if integer > max_integer:
+            max_integer = integer
 
-  return max_integer
+    return max_integer
+
