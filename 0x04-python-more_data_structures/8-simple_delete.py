7f7b43f373d02ef333e551090795e0b789477164
Anthony Ndolo <bondolo90@gmail.com>
2023-06-14 12:06:36 -0700

    Revise no. 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary: del a_dictionary[key]
-    return a_dictionary
+    new_dictionary = dict(a_dictionary)
+    if key in new_dictionary:
+        del new_dictionary[key]
+    return new_dictionary
