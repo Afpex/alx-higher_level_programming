a278a27ce54e39a02d9b28c4faa21bac8980e7ca
Anthony <bondolo90@gmail.com>
2023-09-29 03:47:40 -0700

    adding error handling -> fetching a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" A script that fetches https://alx-intranet.hbtn.io/status using the urlib package
+""" A script that fetches https://alx-intranet.hbtn.io/status using
+the urlib package
 This script sends an HTTP GET request to the specified url and displays
 information about the response
 """
