dc3eb08f18d78946070bb2c7f83047f675ac45a9
Anthony <bondolo90@gmail.com>
2023-09-29 03:38:14 -0700

    adding the main construct to -> fetching a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-# script that fetches https://alx-intranet.hbtn.io/status
+""" A script that
+- fetches https://alx-intranet.hbtn.io/status
+- using the urlib package
+"""
 
-import urllib.request
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    content = response.read().decode('utf-8')
-print("-" * 20)
-print("Body response:")
-print("\t- type: {}".format(type(content)))
-print("\t- content: {}".format(content))
-print("\t- utf8 content: {}".format(content))
-print("-" * 20)
+if __name__ == "__main__":
+    import urllib.request
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read().decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content))
