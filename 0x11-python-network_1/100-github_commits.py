345a8301aa5c0dbc0dc71c278d1d14bba0e56a7a
Anthony <bondolo90@gmail.com>
2023-09-29 08:32:38 -0700

    listing recent commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script that takes 2 arguments in order to list the 10 most recent
+commits (from the most recent to oldest) of the repository "rails" by
+the user "rails".
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        repo_name = sys.argv[1]
+        owner_name = sys.argv[2]
+        commits_url = "https://api.github.com/repos/{}/{}/commits" \
+                .format(owner_name, repo_name)
+        response = requests.get(commits_url)
+        json_obj = response.json()
+        for k, obj in enumerate(json_obj):
+            if k == 10:
+                break
+            if type(obj) is dict:
+                name = obj.get('commit').get('author').get('name')
+                print("{}: {}".format(obj.get('sha'), name))
+        except ValueError as invalid_json:
+            pass
